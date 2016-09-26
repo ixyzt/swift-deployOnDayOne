532f508274fdf52b24a38e454e98b04a25d2a77f
wsrobins <wsrobins@uvm.edu>
2016-09-26 12:01:26 -0400

    ui actually werking now

--- ticTacToe/ticTacToe/ViewController.swift
+++ ticTacToe/ticTacToe/ViewController.swift
@@ -10,46 +10,107 @@ import UIKit
 
 class ViewController: UIViewController {
     var numberOfTurns = 0
-    var buttonsAlreadyPressed = [[String]]()
-
-    @IBOutlet weak var gameButton1x1: UIButton!
-    @IBOutlet weak var gameButton1x2: UIButton!
-    @IBOutlet weak var gameButton1x3: UIButton!
-    @IBOutlet weak var gameButton2x1: UIButton!
-    @IBOutlet weak var gameButton2x2: UIButton!
-    @IBOutlet weak var gameButton2x3: UIButton!
-    @IBOutlet weak var gameButton3x1: UIButton!
-    @IBOutlet weak var gameButton3x2: UIButton!
-    @IBOutlet weak var gameButton3x3: UIButton!
+    var buttonsAlreadyPressed = [["", "", ""], ["", "", ""], ["", "", ""]]
+    
+    @IBOutlet weak var label1: UILabel!
+    @IBOutlet weak var label2: UILabel!
+    @IBOutlet weak var label3: UILabel!
+    @IBOutlet weak var label4: UILabel!
+    @IBOutlet weak var label5: UILabel!
+    @IBOutlet weak var label6: UILabel!
+    @IBOutlet weak var label7: UILabel!
+    @IBOutlet weak var label8: UILabel!
+    @IBOutlet weak var label9: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-    func displayMove(moveCharacter: String, nameOfButton: String) {
-        switch nameOfButton {
-        case "1x1"
+    func getMove() -> String {
+        if numberOfTurns % 2 == 0{
+            return "X"
+        } else {
+            return "O"
         }
     }
     
     func gameIsOver() {
-        
     }
-
-    @IBAction func gameSquarePressed(_ sender: AnyObject) {
+    
+    
+    @IBAction func gameButtonPressed(_ sender: AnyObject) {
+        var currentMove: String
         
-        if numberOfTurns == 9 {
-            gameIsOver()
-        } else if numberOfTurns % 2 == 0 {
-            displayMove(moveCharacter: "X", nameOfButton: sender.title)
-        } else {
-            displayMove(moveCharacter: "O", sender.title)
+        switch sender.tag {
+        case 1:
+            if buttonsAlreadyPressed[0][0] == "" {
+                currentMove = getMove()
+                label1.text = currentMove
+                buttonsAlreadyPressed[0][0] = currentMove
+                numberOfTurns += 1
+            }
+        case 2:
+            if buttonsAlreadyPressed[0][1] == "" {
+                currentMove = getMove()
+                label2.text = currentMove
+                buttonsAlreadyPressed[0][1] = currentMove
+                numberOfTurns += 1
+            }
+        case 3:
+            if buttonsAlreadyPressed[0][2] == "" {
+                currentMove = getMove()
+                label3.text = currentMove
+                buttonsAlreadyPressed[0][2] = currentMove
+                numberOfTurns += 1
+            }
+        case 4:
+            if buttonsAlreadyPressed[1][0] == "" {
+                currentMove = getMove()
+                label4.text = currentMove
+                buttonsAlreadyPressed[1][0] = currentMove
+                numberOfTurns += 1
+            }
+        case 5:
+            if buttonsAlreadyPressed[1][1] == "" {
+                currentMove = getMove()
+                label5.text = currentMove
+                buttonsAlreadyPressed[1][1] = currentMove
+                numberOfTurns += 1
+            }
+        case 6:
+            if buttonsAlreadyPressed[1][2] == "" {
+                currentMove = getMove()
+                label6.text = currentMove
+                buttonsAlreadyPressed[1][2] = currentMove
+                numberOfTurns += 1
+            }
+        case 7:
+            if buttonsAlreadyPressed[2][0] == "" {
+                currentMove = getMove()
+                label7.text = currentMove
+                buttonsAlreadyPressed[2][0] = currentMove
+                numberOfTurns += 1
+            }
+        case 8:
+            if buttonsAlreadyPressed[2][1] == "" {
+                currentMove = getMove()
+                label8.text = currentMove
+                buttonsAlreadyPressed[2][1] = currentMove
+                numberOfTurns += 1
+            }
+        default:
+            if buttonsAlreadyPressed[2][2] == "" {
+                currentMove = getMove()
+                label9.text = currentMove
+                buttonsAlreadyPressed[2][2] = currentMove
+                numberOfTurns += 1
+            }
         }
     }
 }
