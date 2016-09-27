1cfeb4f588c4ef42775f4f061812297934f7d970
wsrobins <wsrobins@uvm.edu>
2016-09-27 01:21:17 -0400

    new ui implemented

--- ticTacToe/ticTacToe/ViewController.swift
+++ ticTacToe/ticTacToe/ViewController.swift
@@ -9,48 +9,48 @@
 import UIKit
 
 class ViewController: UIViewController {
-    var computerPlayer = false
-    
+    var players = ["", ""]
+    var firstPlayer = ""
     var gameWon = false
     var gameTied = false
-    var winningPlayer = ""
-    var numberOfTurns = 0
-    var openSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    var buttonsAlreadyPressed = [["", "", ""], ["", "", ""], ["", "", ""]]
+    var winningPlayer: String?
+    var turnsPlayed = 0
+    var currentMove = ""
+    var selectedSquare = 0
+    var openSquares = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    var movesOnBoard = [String?](repeating: nil, count: 9)
     
-    @IBOutlet weak var label1: UILabel!
-    @IBOutlet weak var label2: UILabel!
-    @IBOutlet weak var label3: UILabel!
-    @IBOutlet weak var label4: UILabel!
-    @IBOutlet weak var label5: UILabel!
-    @IBOutlet weak var label6: UILabel!
-    @IBOutlet weak var label7: UILabel!
-    @IBOutlet weak var label8: UILabel!
-    @IBOutlet weak var label9: UILabel!
-    
-    @IBOutlet weak var turnLabel: UILabel!
-    
-    @IBOutlet weak var pvpButton: UIButton!
-    @IBOutlet weak var pvcButton: UIButton!
-    
-    @IBOutlet weak var boardCoveringButton: UIButton!
-    @IBOutlet weak var playAgainButton: UIButton!
     @IBOutlet var mainView: UIView!
     
+    @IBOutlet weak var gameBoardView: UIView!
+    @IBOutlet weak var boardButton0: UIButton!
+    @IBOutlet weak var boardButton1: UIButton!
+    @IBOutlet weak var boardButton2: UIButton!
+    @IBOutlet weak var boardButton3: UIButton!
+    @IBOutlet weak var boardButton4: UIButton!
+    @IBOutlet weak var boardButton5: UIButton!
+    @IBOutlet weak var boardButton6: UIButton!
+    @IBOutlet weak var boardButton7: UIButton!
+    @IBOutlet weak var boardButton8: UIButton!
+    
+    @IBOutlet weak var turnLabel: UILabel!
+    @IBOutlet weak var moveLabel: UILabel!
+    
+    @IBOutlet weak var playerVsPlayerButton: UIButton!
+    @IBOutlet weak var playerVsComputerButton: UIButton!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         turnLabel.isHidden = true
-        playAgainButton.isHidden = true
-        // Do any additional setup after loading the view, typically from a nib.
+        moveLabel.isHidden = true
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     func getMove() -> String {
-        if numberOfTurns % 2 == 0 {
+        if turnsPlayed % 2 == 0 {
             return "X"
         } else {
             return "O"
@@ -61,10 +61,10 @@ class ViewController: UIViewController {
         if gameWon {
             turnLabel.text = "\(winningPlayer) wins!"
         } else {
-            if numberOfTurns == 9 {
+            if turnsPlayed == 9 {
                 gameTied = true
                 turnLabel.text = "It's a tie!"
-            } else if numberOfTurns % 2 == 0 {
+            } else if turnsPlayed % 2 == 0 {
                 turnLabel.text = "Player 1's Turn"
             } else {
                 if computerPlayer { // fix this and insert short delay for computer turn
@@ -80,76 +80,76 @@ class ViewController: UIViewController {
         var currentMove: String
         switch chosenSquare {
         case 1:
-            if buttonsAlreadyPressed[0][0] == "" {
+            if gameBoard[0][0] == "" {
                 currentMove = getMove()
                 label1.text = currentMove
-                buttonsAlreadyPressed[0][0] = currentMove
+                gameBoard[0][0] = currentMove
                 openSquares.remove(at: openSquares.index(of: 1)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         case 2:
-            if buttonsAlreadyPressed[0][1] == "" {
+            if gameBoard[0][1] == "" {
                 currentMove = getMove()
                 label2.text = currentMove
-                buttonsAlreadyPressed[0][1] = currentMove
+                gameBoard[0][1] = currentMove
                 openSquares.remove(at: openSquares.index(of: 2)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         case 3:
-            if buttonsAlreadyPressed[0][2] == "" {
+            if gameBoard[0][2] == "" {
                 currentMove = getMove()
                 label3.text = currentMove
-                buttonsAlreadyPressed[0][2] = currentMove
+                gameBoard[0][2] = currentMove
                 openSquares.remove(at: openSquares.index(of: 3)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         case 4:
-            if buttonsAlreadyPressed[1][0] == "" {
+            if gameBoard[1][0] == "" {
                 currentMove = getMove()
                 label4.text = currentMove
-                buttonsAlreadyPressed[1][0] = currentMove
+                gameBoard[1][0] = currentMove
                 openSquares.remove(at: openSquares.index(of: 4)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         case 5:
-            if buttonsAlreadyPressed[1][1] == "" {
+            if gameBoard[1][1] == "" {
                 currentMove = getMove()
                 label5.text = currentMove
-                buttonsAlreadyPressed[1][1] = currentMove
+                gameBoard[1][1] = currentMove
                 openSquares.remove(at: openSquares.index(of: 5)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         case 6:
-            if buttonsAlreadyPressed[1][2] == "" {
+            if gameBoard[1][2] == "" {
                 currentMove = getMove()
                 label6.text = currentMove
-                buttonsAlreadyPressed[1][2] = currentMove
+                gameBoard[1][2] = currentMove
                 openSquares.remove(at: openSquares.index(of: 6)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         case 7:
-            if buttonsAlreadyPressed[2][0] == "" {
+            if gameBoard[2][0] == "" {
                 currentMove = getMove()
                 label7.text = currentMove
-                buttonsAlreadyPressed[2][0] = currentMove
+                gameBoard[2][0] = currentMove
                 openSquares.remove(at: openSquares.index(of: 7)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         case 8:
-            if buttonsAlreadyPressed[2][1] == "" {
+            if gameBoard[2][1] == "" {
                 currentMove = getMove()
                 label8.text = currentMove
-                buttonsAlreadyPressed[2][1] = currentMove
+                gameBoard[2][1] = currentMove
                 openSquares.remove(at: openSquares.index(of: 8)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         case 9:
-            if buttonsAlreadyPressed[2][2] == "" {
+            if gameBoard[2][2] == "" {
                 currentMove = getMove()
                 label9.text = currentMove
-                buttonsAlreadyPressed[2][2] = currentMove
+                gameBoard[2][2] = currentMove
                 openSquares.remove(at: openSquares.index(of: 9)!)
-                numberOfTurns += 1
+                turnsPlayed += 1
             }
         default:
             break
@@ -175,22 +175,22 @@ class ViewController: UIViewController {
     
     func getColumns() -> [[String]] {
         return [
-            [buttonsAlreadyPressed[0][0], buttonsAlreadyPressed[1][0], buttonsAlreadyPressed[2][0]],
-            [buttonsAlreadyPressed[0][1], buttonsAlreadyPressed[1][1], buttonsAlreadyPressed[2][1]],
-            [buttonsAlreadyPressed[0][2], buttonsAlreadyPressed[1][2], buttonsAlreadyPressed[2][2]]
+            [movesOnBoard[0][0], gameBoard[1][0], gameBoard[2][0]],
+            [gameBoard[0][1], gameBoard[1][1], gameBoard[2][1]],
+            [gameBoard[0][2], gameBoard[1][2], gameBoard[2][2]]
         ]
     }
     
     func getDiagonals() -> [[String]] {
         return [
-            [buttonsAlreadyPressed[0][0], buttonsAlreadyPressed[1][1], buttonsAlreadyPressed[2][2]],
-            [buttonsAlreadyPressed[0][2], buttonsAlreadyPressed[1][1], buttonsAlreadyPressed[2][0]]
+            [movesOnBoard[0][0], gameBoard[1][1], gameBoard[2][2]],
+            [gameBoard[0][2], gameBoard[1][1], gameBoard[2][0]]
         ]
     }
     
     func isGameOver() {
         let sequences = [
-            "rows" : buttonsAlreadyPressed,
+            "rows" : movesOnBoard,
             "columns" : getColumns(),
             "diagonals" : getDiagonals()
         ]
@@ -222,22 +222,36 @@ class ViewController: UIViewController {
         playAgainButton.isHidden = false
     }
     
-    @IBAction func pvpButtonPressed(_ sender: AnyObject) {
-        changeLabel()
-        computerPlayer = false
+    func startGame() {
+        playerVsPlayerButton.isHidden = true
+        playerVsComputerButton.isHidden = true
+        firstPlayer = players[Int(arc4random_uniform(2))]
+        currentMove = getMove()
+        turnLabel.text = "\(firstPlayer)'s Turn"
+        moveLabel.text = currentMove
         turnLabel.isHidden = false
-        pvpButton.isHidden = true
-        pvcButton.isHidden = true
-        boardCoveringButton.isHidden = true
+        moveLabel.isHidden = false
+        if firstPlayer == "Computer" {
+            makeComputerMove()
+        }
     }
     
-    @IBAction func pvcButtonPressed(_ sender: AnyObject) {
-        changeLabel()
-        computerPlayer = true
-        turnLabel.isHidden = false
-        pvpButton.isHidden = true
-        pvcButton.isHidden = true
-        boardCoveringButton.isHidden = true
+    func makeComputerMove() {
+        mainView.isUserInteractionEnabled = false
+        self.selectedSquare = self.openSquares[Int(arc4random_uniform(UInt32(self.openSquares.count)))]
+        })
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.2, execute: {
+            self.mainView.isUserInteractionEnabled = true
+        })
+    }
+    
+    @IBAction func gameTypeButtonPressed(_ sender: AnyObject) {
+        if sender.title == "Player vs. Player" {
+            players = ["Player One", "Player Two"]
+        } else {
+            players = ["Player", "Computer"]
+        }
+        startGame()
     }
     
     @IBAction func gameButtonPressed(_ sender: AnyObject) {
@@ -250,7 +264,7 @@ class ViewController: UIViewController {
         }
         
         // computer
-        if !gameWon && computerPlayer && numberOfTurns < 9 {
+        if !gameWon && computerPlayer && turnsPlayed < 9 {
             makeMove(chosenSquare: openSquares[Int(arc4random_uniform(UInt32(openSquares.count)))])
             isGameOver()
             changeLabel()
@@ -261,28 +275,28 @@ class ViewController: UIViewController {
         }
     }
     
-    @IBAction func newGame(_ sender: AnyObject) {
-        playAgainButton.isHidden = true
-        boardCoveringButton.isHidden = false
-        pvcButton.isHidden = false
-        pvpButton.isHidden = false
-        turnLabel.isHidden = true
-        computerPlayer = false
-        gameWon = false
-        gameTied = false
-        winningPlayer = ""
-        numberOfTurns = 0
-        openSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        buttonsAlreadyPressed = [["", "", ""], ["", "", ""], ["", "", ""]]
-        label1.text = ""
-        label2.text = ""
-        label3.text = ""
-        label4.text = ""
-        label5.text = ""
-        label6.text = ""
-        label7.text = ""
-        label8.text = ""
-        label9.text = ""
-    }
+//    @IBAction func newGame(_ sender: AnyObject) {
+//        playAgainButton.isHidden = true
+//        boardCoveringButton.isHidden = false
+//        pvcButton.isHidden = false
+//        pvpButton.isHidden = false
+//        turnLabel.isHidden = true
+//        computerPlayer = false
+//        gameWon = false
+//        gameTied = false
+//        winningPlayer = ""
+//        turnsPlayed = 0
+//        openSquares = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+//        gameBoard = [["", "", ""], ["", "", ""], ["", "", ""]]
+//        label1.text = ""
+//        label2.text = ""
+//        label3.text = ""
+//        label4.text = ""
+//        label5.text = ""
+//        label6.text = ""
+//        label7.text = ""
+//        label8.text = ""
+//        label9.text = ""
+//    }
 }
 
